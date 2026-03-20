4c066433a680d2a71809651ebcb20d502708500f
Nikola Vukosavljevic <vukosavljevicn.va@gmail.com>
2026-03-20 16:26:39 +0100

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider, CssBaseline, Box } from "@mui/material";
 import { theme } from "../theme";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box
-        sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
-      >
-        <Navbar />
-      </Box>
+      <BrowserRouter>
+        <Box
+          sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+        >
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
